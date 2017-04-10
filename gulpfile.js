517795433a3b096e99eed71f2b56a4cf2b1ba296
noumenonnn <dev.vlsp@gmail.com>
2017-04-10 17:44:47 +0300

    Configure gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,37 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs'),
-    cssnano = require('gulp-cssnano'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    cache = require('gulp-cache'),
-    autoprefixer = require('gulp-autoprefixer'),
-    iconFont = require('gulp-iconfont'),
+var gulp       = require('gulp'), // Подключаем Gulp
+    sass         = require('gulp-sass'), //Подключаем Sass пакет,
+    browserSync  = require('browser-sync'), // Подключаем Browser Sync
+    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
+    autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
+    iconFont     = require('gulp-iconfont'),
     runTimestamp = Math.round(Date.now() / 1000),
-    async = require('async'),
-    consolidate = require("gulp-consolidate");
+    async        = require('async'),
+    consolidate  = require("gulp-consolidate"),
+    realFavicon  = require('gulp-real-favicon'),
+    svgmin       = require('gulp-svgmin'),
+    fs           = require('fs'),
 
-gulp.task('sass', function() { // Создаем таск Sass
-    return gulp.src('app/scss/**/*.scss'). // Берем источник
-    pipe(sass()). // Преобразуем Scss в CSS посредством gulp-sass
-    pipe(autoprefixer([
-        'last 15 versions', '> 1%', 'ie 8', 'ie 7'
-    ], {cascade: true})). // Создаем префиксы
-    pipe(gulp.dest('app/css')). // Выгружаем результата в папку app/css
-    pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-});
+    FAVICON_DATA_FILE = 'faviconData.json'; // File where the favicon markups are stored
 
-gulp.task('css-libs', ['sass'], function() {
-    return gulp.src('app/css/main.css'). // Выбираем файл для минификации
-    pipe(cssnano()). // Сжимаем
-    pipe(rename({suffix: '.min'})). // Добавляем суффикс .min
-    pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
-});
 
-// TODO: Настроить экспорт иконок из svg в шрифт 
-gulp.task('Iconfont', function(done) {
-    var iconStream = gulp.src(['app/img/icons/*.svg']).pipe(iconfont({fontName: 'myfont'}));
-
-    async.parallel([
-        function handleGlyphs(cb) {
-            iconStream.on('glyphs', function(glyphs, options) {
-                gulp.src('templates/myfont.css').pipe(consolidate('lodash', {
-                    glyphs: glyphs,
-                    fontName: 'myfont',
-                    fontPath: '../fonts/',
-                    className: 's'
-                })).pipe(gulp.dest('www/css/')).on('finish', cb);
-            });
-        },
-        function handleFonts(cb) {
-            iconStream.pipe(gulp.dest('www/fonts/')).on('finish', cb);
-        }
-    ], done);
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('app/scss/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 16 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({
-        server: {
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
             baseDir: 'app' // Директория для сервера - app
         },
         notify: false // Отключаем уведомления
@@ -63,61 +39,192 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 });
 
 gulp.task('scripts', function() {
-    return gulp.src([
-        'app/libs/jquery/dist/jquery.min.js'; //jQuery
-        // Другие сторонние библиотеки
-    ]).pipe(concat('libs.min.js')). // Собираем их в новом файле libs.min.js
-    pipe(uglify()). // Сжимаем JS файл
-    pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+    return gulp.src([ // Берем все необходимые библиотеки
+        'app/libs/jquery/dist/jquery.min.js' // Берем jQuery
+        //other libraries
+        ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
 });
 
-gulp.task('watch', [
-    'browser-sync', 'css-libs', 'scripts'
-], function() {
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/main.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
+
+gulp.task('Iconfont', function(done){
+  var iconStream = gulp.src(['app/img/icons/*.svg'])
+    .pipe(iconFont({ fontName: 'myIcons' }));
+
+  async.parallel([
+    function handleGlyphs (cb) {
+      iconStream.on('glyphs', function(glyphs, options) {
+        gulp.src('app/css/my-icons.scss')
+          .pipe(consolidate('lodash', {
+            glyphs: glyphs,
+            fontName: 'myIcons',
+            fontPath: 'app/fonts/',
+            className: 's',
+            // fontRound: 2000,
+            normalize: true,
+            fontHeight: 1001
+
+          }))
+          .pipe(gulp.dest('app/css/'))
+          .on('finish', cb);
+      });
+    },
+    function handleFonts (cb) {
+      iconStream
+        .pipe(gulp.dest('app/fonts/'))
+        .on('finish', cb);
+    }
+  ], done);
+});
+
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', function(done) {
+	realFavicon.generateFavicon({
+		masterPicture: 'app/img/icons/favicon.png',
+		dest: 'app/img/icons/favicons/',
+		iconsPath: 'app/img/icons/favicons/',
+		design: {
+			ios: {
+				pictureAspect: 'noChange',
+				assets: {
+					ios6AndPriorIcons: false,
+					ios7AndLaterIcons: false,
+					precomposedIcons: false,
+					declareOnlyDefaultIcon: true
+				},
+				appName: 'Stefan Burger'
+			},
+			desktopBrowser: {},
+			windows: {
+				pictureAspect: 'noChange',
+				backgroundColor: '#0c311a',
+				onConflict: 'override',
+				assets: {
+					windows80Ie10Tile: false,
+					windows10Ie11EdgeTiles: {
+						small: false,
+						medium: true,
+						big: false,
+						rectangle: false
+					}
+				},
+				appName: 'Stefan Burger'
+			},
+			androidChrome: {
+				pictureAspect: 'shadow',
+				themeColor: '#ffffff',
+				manifest: {
+					name: 'Stefan Burger',
+					display: 'standalone',
+					orientation: 'notSet',
+					onConflict: 'override',
+					declared: true
+				},
+				assets: {
+					legacyIcon: false,
+					lowResolutionIcons: false
+				}
+			},
+			safariPinnedTab: {
+				pictureAspect: 'blackAndWhite',
+				threshold: 31.25,
+				themeColor: '#0c311a'
+			}
+		},
+		settings: {
+			compression: 4,
+			scalingAlgorithm: 'Cubic',
+			errorOnImageTooSmall: false
+		},
+		markupFile: FAVICON_DATA_FILE
+	}, function() {
+		done();
+	});
+});
+
+// Inject the favicon markups in your HTML pages. You should run
+// this task whenever you modify a page. You can keep this task
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', function() {
+	return gulp.src([ 'app/**/*.html' ])
+		.pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+		.pipe(gulp.dest('app'));
+});
+
+
+gulp.task('svg', function(){
+  return gulp.src(['app/img/icons/**/*.svg'])
+      .pipe(svgmin())
+      .pipe(gulp.dest('app/img/icons'));
+});
+
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your
+// continuous integration system.
+gulp.task('check-for-favicon-update', function(done) {
+	var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+	realFavicon.checkForUpdates(currentVersion, function(err) {
+		if (err) {
+			throw err;
+		}
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'svg', 'scripts'], function() {
     gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-    gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+    gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+    gulp.watch('app/img/icons/**/*.svg');
 });
 
-gulp.task('clean', function() { //Отчистка папки продакшена
-    return del.sync('dist');
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
 });
 
 gulp.task('img', function() {
-    return gulp.src('app/img/**/*'). // Берем все изображения из app
-    pipe(cache(imagemin({ // Сжимаем их с наилучшими настройками с учетом кеширования
-        interlaced: true,
-        progressive: true,
-        svgoPlugins: [
-            {
-                removeViewBox: false
-            }
-        ],
-        use: [pngquant()]
-    }))).pipe(gulp.dest('dist/img'))
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
 });
 
-gulp.task('build', [
-    'clean', 'sass', 'img', 'scripts'
-], function() { // Сборка проекта
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
 
-    var buildCss = gulp.src([ // Переносим CSS стили в продакшен
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
         'app/css/main.css',
         'app/css/libs.min.css'
-    ]).pipe(gulp.dest('dist/css'))
+        ])
+    .pipe(gulp.dest('dist/css'))
 
-    var buildFonts = gulp.src('app/fonts/**/*'). // Переносим шрифты в продакшен
-    pipe(gulp.dest('dist/fonts'))
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/fonts'))
 
-    var buildJs = gulp.src('app/js/**/*'). // Переносим скрипты в продакшен
-    pipe(gulp.dest('dist/js'))
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/js'))
 
-    var buildHtml = gulp.src('app/*.html'). // Переносим HTML в продакшен
-    pipe(gulp.dest('dist'));
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
 });
 
-gulp.task('clear', function() {
+gulp.task('clear', function () {
     return cache.clearAll();
-});
+})
 
 gulp.task('default', ['watch']);
